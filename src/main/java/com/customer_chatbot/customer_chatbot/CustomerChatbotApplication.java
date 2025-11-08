e259fa0c4d19e93e4e9b685903617824f05d19f8
pralhad gadlegave <pralhadgadlegave55@gmail.com>
2025-11-08 22:34:22 +0530

    Initial commit: Customer ChatBot Spring Boot project

---
+++ src/main/java/com/customer_chatbot/customer_chatbot/CustomerChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.customer_chatbot.customer_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer_chatbot/customer_chatbot/CustomerChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer_chatbot.customer_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
